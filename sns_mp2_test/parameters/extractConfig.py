4b39486524c9c10ad4ea15bfea89c482803b8167
Uper <1404293933@qq.com>
2022-04-29 11:54:50 +0800

    量子力学的提出

--- sns_mp2_test/parameters/extractConfig.py
+++
@@ -1,7 +0,0 @@
-'''
-Part I
-------
-    The config for `FormulaExtractor`
-'''
-DES_csv_path = "/data/home/liuhanyu/Interaction/DES_dataset/DES370K.csv"
-row = 100000
--- sns_mp2_test/parameters/inputFileConfig.py
+++
@@ -1,2 +0,0 @@
-# 生成的 psi4 的输入文件的路径
-input_file_psi4_path = "/data/home/liuhanyu/Interaction/workdir/demo_3/input.dat"
