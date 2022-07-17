0f7b47cc08bed47b4ef2260226e9bbec98beecbe
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-07-17 22:55:54 +0800

    Proof: The magnitude of unitary operator is 1

--- MD/1.basics/code/test.cpp
+++ MD/1.basics/code/test.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: Uper 41718895+Hyliu-BUAA@users.noreply.github.com
+ * @Date: 2022-06-25 14:35:41
+ * @LastEditors: Uper 41718895+Hyliu-BUAA@users.noreply.github.com
+ * @LastEditTime: 2022-07-14 22:38:26
+ * @FilePath: /Quantum_Mechanics/MD/1.basics/code/test.cpp
+    * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
@@ -7,12 +15,12 @@ int main()
 {
     // This program will create different sequence of
     // random numbers on every program run
- 
+
     // Use current time as seed for random generator
     srand(time(0));
- 
+
     for(int i = 0; i < 4; i++)
         std::cout << rand() << std::endl;
- 
+
     return 0;
 }
