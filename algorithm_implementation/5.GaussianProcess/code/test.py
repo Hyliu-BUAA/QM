402d30d3901a4b142bf28365989f82109e0852a2
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-10-02 15:23:38 +0800

    Implementation of gaussian kernel function

--- algorithm_implementation/5.GaussianProcess/code/test.py
+++ algorithm_implementation/5.GaussianProcess/code/test.py
@@ -1,31 +1,31 @@
+'''
+Author       : Liu Hanyu
+Email        : hyliu2016@buaa.edu.cn
+Date         : 2022-09-30 13:42:09
+LastEditTime : 2022-10-02 15:21:55
+FilePath     : /Quantum_Mechanics/algorithm_implementation/5.GaussianProcess/code/test.py
+Description  : 
+'''
 import numpy as np
 
 
-class GPR(object):
-    def __init__(self, optimize:bool=True):
-        self.is_fit = False
-        self.train_X, self.train_y = None, None
-        self.params = {"l": 0.5, "sigma_f": 0.2}    # 500 10
-        self.optimize = optimize
-    
 
-    def fit(self, X, y):
-        # store
-        self.train_X = np.asarray(X)
-        self.train_y = np.asarray(y)
-        self.is_fit = True
+def gaussian_kernel(x1:np.array,
+                    x2:np.array,
+                    l:float=1.0,
+                    sigma_f=1.0):
+    '''
+    Note:
+    ----
+        1. x1, x2 都需要以列向量形式输入 (x1, x2 都是二维 np.array)
+    '''
+    dist_matrix = np.sum(np.power(x1, 2), 1).reshape(-1, 1) + \
+                np.sum(np.power(x2, 2), 1).reshape(1, -1) - \
+                2 * x1 * x2.T
+    return np.power(sigma_f, 2) * np.exp(-0.5 * dist_matrix / np.power(l, 2))
 
-    
-    def kernel(self, x1, x2):
-        dist_matrix = np.sum( np.power(x1, 2), 1 ).reshape(-1, 1) + \
-                        np.sum( np.power(x2, 2), 1 ).reshape(1, -1) - \
-                        2 * x1 * x2.T
-        return self.params["sigma_f"] ** 2 * np.exp(-0.5 / self.params["l"] ** 2 * dist_matrix)
 
 
 if __name__ == "__main__":
     x = np.array([700, 800, 1029]).reshape(-1, 1)
-    gpr = GPR()
-    gpr.params["l"] = 500
-    gpr.params["sigma_F"] = 10
-    print( gpr.kernel(x, x) )
+    print(gaussian_kernel(x, x, l=500, sigma_f=10))
