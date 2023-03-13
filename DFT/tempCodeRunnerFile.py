0b21ecdd66ff7370dd60f6aeaa72ecd5156ba3fd
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2023-03-13 17:57:16 +0800

    How to calculate TDOS from WAVECAR

---
+++ DFT/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+Define the lattice constant
--- DFT/test.py
+++ DFT/test.py
@@ -1,12 +1,3 @@
-'''
-Author       : Liu Hanyu
-Email        : hyliu2016@buaa.edu.cn
-Date         : 2022-09-08 21:48:47
-LastEditTime : 2022-09-08 21:51:02
-FilePath     : /Quantum_Mechanics/DFT/test.py
-Description  : 
-'''
-from ase.dft.kpoints import monkhorst_pack, get_monkhorst_pack_size_and_offset
+import numpy as np
 
-print( monkhorst_pack((4, 2, 1)) )
-print( get_monkhorst_pack_size_and_offset([[0, 0, 0]]) )
+wavecar_path = ""
